feb6d9bc0d1154962e2cb2de4d28f049526547cb
程艳晓 <chengyanxiao>
2019-09-04 18:42:15 +0800

    初始提交

---
+++ javabasic/src/main/java/com/example/javabasic/java8/Apple.java
@@ -0,0 +1,17 @@
+package com.example.javabasic.java8;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author：Cheng.
+ * @date：Created in 18:37 2019/9/4
+ */
+@Setter
+@Getter
+@Data
+@ToString
+public class Apple {
+}
