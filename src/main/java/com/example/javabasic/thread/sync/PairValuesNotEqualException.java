899e5acaab13537be5e871d262f5be06fd793307
Garcia11 <erya0923!>
2019-12-03 23:20:18 +0800

    初始提交

--- src/main/java/com/example/javabasic/thread/sync/PairValuesNotEqualException.java
+++ src/main/java/com/example/javabasic/thread/sync/PairValuesNotEqualException.java
@@ -15,6 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 class Pair{
 
+    //Pair不是线程安全的，因为其约束条件需要两个变量要维护成相同的值。
     private int x, y;
 
     public Pair(int x,int y){
@@ -34,6 +35,9 @@ class Pair{
         return y;
     }
 
+    /**
+     * 自增加操作不是线程安全的，需要从主内存中取，然后从本地内存写回主内存
+     */
     public void incrementX(){
         x++;
     }
@@ -65,12 +69,19 @@ class Pair{
 
 
 /**
+ * PairManager类持有一个Pair对象，并控制对它的一切访问，保证在一个线程安全的环境中使用Pair对象
+ * 模板方法设计模式：一些功能在基类中实现，并且其一个或多个抽象方法在派生类中定义。
  * 虚拟类持有一个同步的Pair List
  */
 abstract class PairManager{
     AtomicInteger checkCounter = new AtomicInteger(0);
     protected Pair p = new Pair();
     private List<Pair> storage = Collections.synchronizedList(new ArrayList<Pair>());
+
+    /**
+     * 唯一的public方法是getPair()，是synchronized修饰的。
+     * @return
+     */
     public synchronized Pair getPair(){
         return new Pair(p.getX(),p.getY());
     }
@@ -92,7 +103,8 @@ class PairManager1 extends PairManager{
     @Override
     public synchronized void increment() {
         p.incrementX();
-        p.incrementY();store(getPair());
+        p.incrementY();
+        store(getPair());
     }
 }
 
@@ -101,11 +113,13 @@ class PairManager2 extends PairManager{
     @Override
     public void increment() {
         Pair temp;
+        //宁愿使用同步控制块，而不是对整个方法进行同步控制的典型原因:使得其他线程能够更多的访问
         synchronized (this){
             p.incrementX();
             p.incrementY();
             temp = getPair();
         }
+        //此方法已经用synchronized修饰，是线程安全的，因此可以放在同步代码块外面
         store(temp);
     }
 }
@@ -124,6 +138,29 @@ class PairManipulator implements Runnable{
     public void run() {
         while (true){
             pm.increment();
+        }
+    }
+
+
+    public String toString(){
+        return "Pair:"+ pm.getPair() +" checkCounter = "+pm.checkCounter.get();
+    }
+}
+
+
+class PairChecker implements Runnable{
+
+    private PairManager pm;
+
+    public PairChecker(PairManager pm){
+        this.pm = pm;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            pm.checkCounter.incrementAndGet();
+            pm.getPair().checkState();
         }
     }
 }
