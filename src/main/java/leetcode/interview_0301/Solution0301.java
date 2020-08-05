0f2a693949955340f58c02687693cd86c41c0908
chengxiao <780960172@qq.com>
2020-08-05 23:41:43 +0800

    leetcode：使用一个数组实现三个栈

--- src/main/java/leetcode/interview_0301/Solution0301.java
+++ src/main/java/leetcode/interview_0301/Solution0301.java
@@ -7,69 +7,77 @@ package leetcode.interview_0301;
 public class Solution0301 {
 
     public static void main(String[] args) {
-        TripleInOne tripleInOne = new TripleInOne(18);
-        System.out.println(tripleInOne.peek(1));
+        TripleInOne tripleInOne = new TripleInOne(2);
 
+        tripleInOne.push(0,1);
         tripleInOne.push(0,2);
+        tripleInOne.push(0,3);
         int result1= tripleInOne.pop(0);
         System.out.println(result1);
         int result2 = tripleInOne.pop(0);
         System.out.println(result2);
         int result3 = tripleInOne.pop(0);
         System.out.println(result3);
-        boolean emptyResult = tripleInOne.isEmpty(1);
-        System.out.println(emptyResult);
+        int result4= tripleInOne.peek(0);
+        System.out.println(result4);
     }
 }
 
 
 /**
- * 使用数组来实现栈
+ * 使用数组来实现三个栈  三个栈 三个栈
  */
 class TripleInOne{
 
+    private int[] arr;//存放三个栈的所有元素
+    private int[] stackTop; // 每个栈当前可push的索引（对应到arr中的索引）
+    private int stackSize;//栈大小
 
-    int[] stackArray;
-    int count = 0;
-
-    public TripleInOne(int statckSize){
-        stackArray = new int[statckSize];
+    public TripleInOne(int stackSize) {
+        this.stackSize = stackSize;
+        arr = new int[stackSize * 3];
+        stackTop = new int[]{0, 1, 2};//存放的是三个栈的栈顶元素在arr中的索引位置
     }
 
-
-    public void push(int statckNum, int value){
-        if(count >= stackArray.length){
-        }else {
-            stackArray[count] = value;
-            count++;
+    /**
+     *
+     * @param stackNum 向哪个栈中添加元素
+     * @param value 待添加的值
+     */
+    public void push(int stackNum, int value) {
+        int curStackTop = stackTop[stackNum];//得到当前下标对应的栈的栈顶下标
+        if (curStackTop / 3 == stackSize) {
+            // 栈已满
+            return;
         }
+        arr[curStackTop] = value;//将这个元素加入当前栈的栈顶
+        stackTop[stackNum] += 3;//栈顶下标+3
     }
 
 
-    public int pop(int statckNum){
-        if(count>0){
-            int result = stackArray[count-1];
-            stackArray[count-1] = 0;
-            count--;
-            return result;
-        }else{
+    /**
+     * pop哪个栈的元素
+     * @param stackNum 栈的索引位置
+     * @return
+     */
+    public int pop(int stackNum) {
+        if (isEmpty(stackNum)) {//当栈为空时，返回-1
             return -1;
         }
+        int value = arr[stackTop[stackNum] - 3];//删除对应的栈顶元素,前面加入元素的时候加了3，现在要减掉
+        stackTop[stackNum] -= 3;//栈顶下标减小3
+        return value;
     }
 
-
-    public int peek(int statckNum){
-        if(count>0){
-            return stackArray[count-1];
-        }else{
+    public int peek(int stackNum) {
+        if (isEmpty(stackNum)) {
             return -1;
         }
+        return arr[stackTop[stackNum] - 3];
     }
 
     public boolean isEmpty(int stackNum) {
-        if(count>=0){
-            return true;
-        }
-        return false;
+        return stackTop[stackNum] < 3;//当栈顶下标小于3，说明没有元素了
     }
+
 }
